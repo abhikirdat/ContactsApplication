29597282282acc80701e0288fa0060fa824d411d
abhi <abhikirdat1990@gmail.com>
2018-07-11 16:51:50 +0530

    delete all folder

--- dbo.ContactsDB.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Contacts]
-(
-	[ContactID] INT NOT NULL PRIMARY KEY, 
-    [FirstName] NVARCHAR(50) NOT NULL, 
-    [LastName] NVARCHAR(50) NOT NULL, 
-    [EmailID] NVARCHAR(50) NOT NULL, 
-    [MobileNumber] NVARCHAR(50) NOT NULL, 
-    [Status] BIT NOT NULL
-)
